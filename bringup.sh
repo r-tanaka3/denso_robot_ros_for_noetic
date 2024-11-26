6ab4f3bcb41d47f72bff68487a2679f7c27a0e21
r-tanaka3 <tanaka-risako205@g.ecc.u-tokyo.ac.jp>
2024-11-26 17:20:06 +0900

    modified for grape robot

--- bringup.sh
+++ bringup.sh
@@ -1,1 +1,2 @@
 roslaunch denso_robot_bringup vs060_bringup.launch sim:=false ip_address:=192.168.11.100
+roslaunch denso_robot_bringup vs060_bringup.launch sim:=false ip_address:=192.168.179.243 robot_name:=VS060A3-AV6-W4N
